8393176cd36ecc82fb8daabab4bd07fdf7b32f6f
Robby <robbyhooker29@gmail.com>
2022-10-25 21:21:03 +0100

    FIrst Commit

---
+++ mysite/main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import ToDoList, Item
+# Create your views here.
+
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return render(response, "main/list.html", {"ls" : ls})
+
+def home(response):
+    return render(response, "main/home.html", {})
+
+def create(response):
+    return redner(response, "main/create.html", {})
